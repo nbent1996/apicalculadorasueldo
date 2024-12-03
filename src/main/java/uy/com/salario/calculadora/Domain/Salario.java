1d5957832b45a9518667da7c771abfc31c8c577b
Bentancor Rodríguez <e828367@net.in.iantel.com.uy>
2024-12-03 13:40:32 -0300

    Avances en la logica

--- src/main/java/uy/com/salario/calculadora/Domain/Salario.java
+++ src/main/java/uy/com/salario/calculadora/Domain/Salario.java
@@ -2,7 +2,98 @@ package uy.com.salario.calculadora.Domain;
 
 public class Salario {
     
-    private Long nominal;
-    private Long liquido;
+    private Double nominal;
+    private Double liquido;
+    private Double aportesJubilatorios;
+    private Double aportesFonasa;
+    private Double aportesFRL;
+    private Double aportesIRPF;
+    private Double aportesFondoSolidaridad;
+    private Double aportesAdicionalFondoSolidaridad;
+    private Double aportesCJPPU;
+    private Double aportesOtrasDeducciones;
+        
+    public Salario(Double aportesJubilatorios, Double aportesFonasa, double aportesFRL) {
+        this.aportesJubilatorios = aportesJubilatorios;
+        this.aportesFonasa = aportesFonasa;
+        this.aportesFRL = aportesFRL;
+        this.nominal = -1.0;
+        this.liquido = -1.0;
+    }
+    public Salario(Double nominal, Double liquido, Double aportesJubilatorios, Double aportesFonasa, double aportesFRL) {
+        this.nominal = nominal;
+        this.liquido = liquido;
+        this.aportesJubilatorios = aportesJubilatorios;
+        this.aportesFonasa = aportesFonasa;
+        this.aportesFRL = aportesFRL;
+    }
 
+    public Salario(Double aportesIRPF) {
+        this.aportesIRPF = aportesIRPF;
+        this.nominal = -1.0;
+        this.liquido = -1.0;
+    }
+    public Double getNominal() {
+        return nominal;
+    }
+    public void setNominal(Double nominal) {
+        this.nominal = nominal;
+    }
+    public Double getLiquido() {
+        return liquido;
+    }
+    public void setLiquido(Double liquido) {
+        this.liquido = liquido;
+    }
+    public Double getAportesJubilatorios() {
+        return aportesJubilatorios;
+    }
+    public void setAportesJubilatorios(Double aportesJubilatorios) {
+        this.aportesJubilatorios = aportesJubilatorios;
+    }
+    public Double getAportesFonasa() {
+        return aportesFonasa;
+    }
+    public void setAportesFonasa(Double aportesFonasa) {
+        this.aportesFonasa = aportesFonasa;
+    }
+    public Double getAportesFRL() {
+        return aportesFRL;
+    }
+    public void setAportesFRL(Double aportesFRL) {
+        this.aportesFRL = aportesFRL;
+    }
+    public Double getAportesIRPF() {
+        return aportesIRPF;
+    }
+    public void setAportesIRPF(Double aportesIRPF) {
+        this.aportesIRPF = aportesIRPF;
+    }
+    public Double getAportesFondoSolidaridad() {
+        return aportesFondoSolidaridad;
+    }
+    public void setAportesFondoSolidaridad(Double aportesFondoSolidaridad) {
+        this.aportesFondoSolidaridad = aportesFondoSolidaridad;
+    }
+    public Double getAportesAdicionalFondoSolidaridad() {
+        return aportesAdicionalFondoSolidaridad;
+    }
+    public void setAportesAdicionalFondoSolidaridad(Double aportesAdicionalFondoSolidaridad) {
+        this.aportesAdicionalFondoSolidaridad = aportesAdicionalFondoSolidaridad;
+    }
+    public Double getAportesCJPPU() {
+        return aportesCJPPU;
+    }
+    public void setAportesCJPPU(Double aportesCJPPU) {
+        this.aportesCJPPU = aportesCJPPU;
+    }
+    public Double getAportesOtrasDeducciones() {
+        return aportesOtrasDeducciones;
+    }
+    public void setAportesOtrasDeducciones(Double aportesOtrasDeducciones) {
+        this.aportesOtrasDeducciones = aportesOtrasDeducciones;
+    }
+
+  
+    
 }
--- src/main/java/uy/com/salario/calculadora/Services/SalarioService.java
+++ src/main/java/uy/com/salario/calculadora/Services/SalarioService.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import uy.com.salario.calculadora.Domain.Constants;
+import uy.com.salario.calculadora.Domain.Salario;
 
 @Service
 public class SalarioService {
@@ -25,17 +26,52 @@ public class SalarioService {
             return retorno;
         }
 
-        public Long calcularAportesBPS (Long nominal, boolean tieneHijos, boolean tieneConyuge){
-            Integer salarioBPC = nominal.intValue()/Constants.BPC.intValue();
+        public Salario calcularAportesBPS (Long nominal, boolean tieneHijos, boolean tieneConyuge){
+            Double salarioBPC = nominal.doubleValue()/Constants.BPC.doubleValue();
             ArrayList<Double> valoresFonasa = null;
             if(salarioBPC > 2.5)
                 valoresFonasa = Constants.APORTE_FONASA_DESDE_25BPC;
             else{
                 valoresFonasa = Constants.APORTE_FONASA_HASTA_25BPC;
             }
-
-            /*Calculamos porcentaje fonasa*/
             
+            /*Calculamos porcentaje fonasa*/
+            int porcentajeFonasa = -1;
+            if(tieneHijos) porcentajeFonasa+= valoresFonasa.get(2);
+            if(tieneConyuge) porcentajeFonasa += valoresFonasa.get(1);
+
+            //Calcular valores de retorno
+            Double aportesJubilatorios = Math.min(Constants.TOPE_APORTE_JUBILATORIO, nominal) * Constants.APORTE_JUBILATORIO * 0.01;
+            Double aportesFonasa = nominal * porcentajeFonasa * 0.01;
+            Double aporteFrl = nominal * Constants.APORTE_FRL * 0.01;
+            return new Salario(aportesJubilatorios, aportesFonasa, aporteFrl);
         }
+        public Salario calcularAportesIRPF(Salario salario, int factorDeduccionPersonasACargo, int cantHijosSinDiscapacidad, int cantHijosConDiscapacidad){
+            /*
+             * objeto salario debe tener
+             *  salarioNominal,
+                aportesJubilatorios,
+                aportesFONASA,
+                aporteFRL,
+                aportesFondoSolidaridad,
+                adicionalFondoSolidaridad,
+                aportesCJPPU,
+                otrasDeducciones
+             */
+            Double salarioEnBPC = salario.getNominal().doubleValue() / Constants.BPC;
+            Integer tasaDeducciones = null;
+            if(salarioEnBPC>15) tasaDeducciones = Constants.TASA_DEDUCCIONES_DESDE15BPC;
+            else tasaDeducciones = Constants.TASA_DEDUCCIONES_HASTA15BPC;
+
+            /*Calcular si hay que aplicar el aumento a ingresos gravados Seguridad Social*/
+            Double nominal = salario.getNominal();
+            if(salarioEnBPC>10) salario.setNominal(nominal *= 1 + Constants.INCREMENTO_INGRESOS_GRAVADOS.doubleValue() *0.01);
+
+            /*Cantidad deducida del IRPF por los hijos */
+            Long deduccionesHijos = factorDeduccionPersonasACargo * (cantHijosSinDiscapacidad * Constants.DEDUCCION_HIJO_SIN_DISCAPACIDAD +
+                                                                     cantHijosConDiscapacidad * Constants.DEDUCCION_HIJO_CON_DISCAPACIDAD);
+
+        }
+
 
 }
